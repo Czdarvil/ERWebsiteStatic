8cd5eba03628cf46470fdff34abd92293314397a
Ben Greene <ben@solegraphics.com>
2017-01-05 16:08:50 -0800

    refactor: header
    Given that the bottom of the header (the dropdown) uses scripts, only the top part of the header is loaded.
    The bottom part is loaded in the iframe. The WP site will both hide the top part of the header, and add padding to the bottom to keep it's position the same

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ The rest of the page is an iframe that loads from one of three sources:
 * The production site
 
 Ths source is determined by the static page's URL (is it .dev or .com, and does it contain .staging)
+
+All links in the iframe are given the attribute `target="_parent"`
+
+##Issues
+
+The bottom part of the header (the drop down) uses JavaScript for functionality, **SO** instead of loading the scripts into the page...
+
+* The bottom part of the header bar is loaded in the iframe.
+* The WordPress Site checks for the parameret `src='embed'`. If it is present, hide the top part of the header, add a `<base>` tag, and add padding to the dropdown (to account for no top header).
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 	// Get whether this is in dev mode, staging mode OR production mode
 	$domain = $_SERVER['HTTP_HOST'];
-	$iframe_url = 'https://explore.emergencyreporting.com';
+	$iframe_url = 'www.explore.emergencyreporting.com';
 	if( false !== strpos( $domain, '.dev' ) ):
-		$iframe_url = 'http://explore.emergencyreporting.dev';
+		$iframe_url = 'www.explore.emergencyreporting.dev';
 	elseif( flase !== strpos( $domain, 'staging' ) ):
-		$iframe_url = 'https://staging.emergencyreporting.com';
+		$iframe_url = 'staging.emergencyreporting.com';
 	endif;
 ?>
 <!DOCTYPE html>
@@ -16,14 +16,22 @@
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 	</head>
 	<body>
-		<div class="wrapper">
-			<div class="er-fallback-header">
-				<span class="logo-container"><a href="<?php echo $iframe_url; ?>"><img class="logo" src="emergency_reporting_logo.png" alt="Logo" /></a></span>
-				<a class="contact" href=""><i class="fa fa-envelope"></i> Contact Us</a>
-				<a class="phone-number" href=""><i class="fa fa-phone"></i> 1 (866) 773-7678</a>
-				<a href=""><span class="button">LOGIN</span></a>
+		<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
+			<div class="top-nav container">
+				<a class="navbar-brand" href="http://explore.emergencyreporting.dev/" rel="home"><img alt="Emergency Reporting" src="http://explore.emergencyreporting.dev/wp-content/uploads/2015/03/emergency_reporting_logo.png"></a>
+				<div class="pull-right">
+					<a href="http://explore.emergencyreporting.dev/login/" class="navbar-btn btn btn-theme-primary">Login</a>
+				</div>
 			</div>
-			<iframe class="er-page-iframe" src="<?php echo $iframe_url; ?>?src=embed"></iframe>
-		</div>
+			<nav class="menu-container">
+				<div class="container mobile-nav-header">
+					<ul id="menu-contact-links" class="contact-menu">
+						<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-13631"><a href="http://explore.emergencyreporting.dev/contact-us/"><i class="fa fa-envelope"></i> <span class="nav-label">Contact Us</span></a></li>
+						<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-13641"><a href="tel:1-866-773-7678"><i class="fa fa-phone"></i> <span class="nav-label">1 (866) 773-7678</span></a></li>
+					</ul>
+				</div>
+		</header>
+		<iframe class="er-page-iframe" src="<?php echo $iframe_url; ?>?src=embed">
+		</iframe>
 	</body>
 </html>
